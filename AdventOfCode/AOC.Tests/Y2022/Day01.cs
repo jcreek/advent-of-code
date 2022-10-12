c486ce00d363f8324d5cb1f2426faa2aa5466ac2
Josh Creek <jcreek93@gmail.com>
2022-10-12 19:43:53 +0100

    build(2022): Add back in previous changes

--- AdventOfCode/AOC.Tests/Y2022/Day01.cs
+++ AdventOfCode/AOC.Tests/Y2022/Day01.cs
@@ -6,9 +6,15 @@ namespace AOC.Tests.Y2022
         public void Setup()
         {
         }
+        
+        [Test]
+        public void Part1()
+        {
+            Assert.Pass();
+        }
 
         [Test]
-        public void Test1()
+        public void Part2()
         {
             Assert.Pass();
         }
