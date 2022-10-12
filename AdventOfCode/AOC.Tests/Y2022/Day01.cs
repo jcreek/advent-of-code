566be1e2b6923559d00e67c205c03dd447041ded
Josh Creek <jcreek93@gmail.com>
2022-10-12 18:47:54 +0100

    build2022): Add new project structure to use test explorer

---
+++ AdventOfCode/AOC.Tests/Y2022/Day01.cs
@@ -0,0 +1,17 @@
+namespace AOC.Tests.Y2022
+{
+    public class Day01
+    {
+        [Fact]
+        public void Part1()
+        {
+
+        }
+
+        [Fact]
+        public void Part2()
+        {
+
+        }
+    }
+}
