5d101b116f1798d9efde823eccaf9f17ba4dcdf8
Josh Creek <jcreek93@gmail.com>
2022-10-12 19:36:57 +0100

    build(2022): Restructure using NUnit not XUnit

---
+++ AdventOfCode/AOC.Test/Y2022/Day01.cs
@@ -0,0 +1,16 @@
+namespace AOC.Test.Y2022
+{
+    public class Day01
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
