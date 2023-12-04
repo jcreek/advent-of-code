c5b096dbea0625a0d95b12ec11efdb196dc39f70
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-12-04 11:54:27 +0000

    chore(*): Add editorconfig

--- AdventOfCode/DayCreator/DayX.cs
+++ AdventOfCode/DayCreator/DayX.cs
@@ -1,45 +1,46 @@
-using System.Reflection;
+namespace AOC.Tests.YX;
 
-namespace AOC.Tests.YX
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class DayX
 {
-    [TestFixture, Parallelizable(ParallelScope.All)]
-    public class DayX
+    [SetUp]
+    public void Setup()
     {
-        protected string GetThisClassName() { return this.GetType().Name; }
-        private string[] realData;
+        realData = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "YX", "Data",
+            $"{GetThisClassName()}.dat"));
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            realData = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "YX", "Data", $"{GetThisClassName()}.dat"));
-        }
+    protected string GetThisClassName() { return GetType().Name; }
+    private string[] realData;
 
-        [TestCase("blah", 0)]
-        [TestCase(null, 232)] // The actual answer
-        public void Part1(string input, int? expected)
-        {
-            string[] lines = input != null ? new[] { input } : realData;
+    [TestCase("blah", 0)]
+    [TestCase(null, 232)] // The actual answer
+    public void Part1(string input, int? expected)
+    {
+        string[] lines = input != null ? new[] { input } : realData;
+        // string[] lines = input != null ? input.Split("\n") : realData;
 
-            //if (expected != null)
-            //{
-            //    Assert.That(result, Is.EqualTo(expected.Value));
-            //}
+        //if (expected != null)
+        //{
+        //    Assert.That(result, Is.EqualTo(expected.Value));
+        //}
 
-            //Console.WriteLine($"Part 1: {result}");
-        }
+        //Console.WriteLine($"Part 1: {result}");
+    }
 
-        [TestCase("blah", 1)] 
-        [TestCase(null, 1783)] // The actual answer
-        public void Part2(string input, int? expected)
-        {
-            //string[] lines = input != null ? new[] { input } : realData;
+    [TestCase("blah", 1)]
+    [TestCase(null, 1783)] // The actual answer
+    public void Part2(string input, int? expected)
+    {
+        //string[] lines = input != null ? new[] { input } : realData;
+        // string[] lines = input != null ? input.Split("\n") : realData;
 
-            //if (expected != null)
-            //{
-            //    Assert.That(result, Is.EqualTo(expected.Value));
-            //}
+        //if (expected != null)
+        //{
+        //    Assert.That(result, Is.EqualTo(expected.Value));
+        //}
 
-            //Console.WriteLine($"Part 2: {result}");
-        }
+        //Console.WriteLine($"Part 2: {result}");
     }
 }
